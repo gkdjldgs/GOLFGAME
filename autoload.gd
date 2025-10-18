48ed85379fb70fe79ec80fc6b93a215c9c02dd2b
melon <jermaise@gmail.com>
2025-10-17 20:56:13 -0600

    Hitboxes
    I've made it so the hitboxes for the slopes are slightly easier to work with by extending it past the tile a bit.

--- autoload.gd
+++ autoload.gd
@@ -48,7 +48,8 @@ func passes():
 		bases()
 	elif level == 4:
 		get_tree().change_scene_to_file("res://Holenumber5.tscn")
-		level = 5
+		bases()
+	elif level == 5:
 		bases()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:

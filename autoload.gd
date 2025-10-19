4443c3a8e8f4cdd3429ad7396ffaa57b866d51a6
melon <jermaise@gmail.com>
2025-10-19 16:25:16 -0600

    Single bug
    The level wasn't update correctly for one,
    It was prioritizing the wrong thing, just changed the order a lil bit

--- autoload.gd
+++ autoload.gd
@@ -64,9 +64,9 @@ func passes():
 		level = 5
 		bases()
 	elif level == 5:
-		bases()
-		level = 6
 		get_tree().change_scene_to_file("res://Holenumber6.tscn")
+		level = 6
+		bases()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	bases()
